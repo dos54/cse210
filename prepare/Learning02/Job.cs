a44c8bb1cca232ea76a57ea6d10dfe02a474c9fe
dos54 <stevencthomas54@gmail.com>
2024-01-18 20:07:57 -0900

    Learning02 Completed

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Job {
+    public string _company;
+    public string _jobTitle;
+    public int _endYear;
+    public int _startYear;
+
+    public void Display() {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new() {
+            _jobTitle = "Manager",
+            _company = "Apple",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        Resume myResume = new()
+        {
+            _name = "Juan Carlos",
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
